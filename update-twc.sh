7b71d366d4d4a59dff9308c5289de9d867594e1e
Laurens van Alphen <laurens.van.alphen@keenondots.com>
2020-02-02 20:47:08 +0100

    folder change

--- update-twc.sh
+++ update-twc.sh
@@ -14,7 +14,7 @@ confirm() {
 }
 
 # git update
-cd /home/pi/scripts/TWCManager
+cd /home/pi/scripts/git/TWCManager
 
 git fetch
 git diff --name-only origin/master
@@ -25,7 +25,7 @@ rsync -Cavr --progress --delete \
  --exclude=.gitignore \
  --exclude=TWCManagerSettings.txt \
  --exclude=TWCManager.log  \
- /home/pi/scripts/TWCManager/ \
+ /home/pi/scripts/git/TWCManager/ \
  /home/pi/scripts/twc/
 
 cd /home/pi/scripts/twc/
